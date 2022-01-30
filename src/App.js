c388b71e4f7fe225283b7327438f32a0a89f4fd9
Adeiza <alexmercyadeiza@gmail.com>
2022-01-30 12:16:57 +0100

    nft floor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
+
+  const [spin, setSpin] = useState(false);
+
+  const toggleSpinner = () => {
+    setSpin(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="grid bg-gray-100 h-screen place-items-center">
+      {!spin ?
+        <div class="w-full max-w-xl rounded-lg bg-white p-10 space-y-8">
+          <div class="flex items-center justify-between">
+            <div class="font-bold tracking-tight text-blue-800">nftfloor.</div>
+            <img src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.svg" class="w-7" alt="opensea.io" />
+          </div>
+
+          <div>
+            <input type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Wallet Address" />
+          </div>
+          <button class="btn btn-sm btn-outline" onClick={toggleSpinner}>Get floor price</button>
+        </div>
+        :
+        <Spinner />
+      }
     </div>
   );
 }
